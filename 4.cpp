328354843b58ef6ec6edea8ebf26a5a9d99e38a9
harshitverma-dev <theharshitverma53@gmail.com>
2022-11-05 21:08:03 +0530

    for loop, while loop, do-while loop

---
+++ 4.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // ********* loops in c++ ********* //
+    // ********** for loop, while loop , do-while loop ********** //
+    // ******** for loop ******** //
+    for (int i = 0; i <= 20; i++)
+    {
+        cout << i << endl;
+    }
+
+
+    // ************ while loop *********** //
+    int j = 0;
+    while ( j <= 5)
+    {
+        cout << j << endl;
+        j++;
+    }
+
+
+    // ************* do while loop ***************** //
+    int k = 50;
+    do
+    {
+        cout << k << endl;
+        k++;
+    } while (k <= 55);
+    
+    
+    
+    return 0;
+}
