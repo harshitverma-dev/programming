7142aca904568a1670188dfefef240152149b211
harshitverma-dev <theharshitverma53@gmail.com>
2022-11-05 17:40:21 +0530

    reference variable, typecasting, manipulators

--- 2.cpp
+++ 2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 
 int main()
@@ -9,10 +10,23 @@ int main()
     cout << a << endl;
     cout << b << endl;
 
-
     // ************* Typecasting ************* //
     int x = 56;
     float y = 78.89;
     cout << int(y) << endl;
     cout << float(x) << endl;
+
+    // ************* Manipulators *************//
+    // endl, setw
+    // for endl
+    int i = 2;
+    int j = 48;
+    int k = 345;
+    cout << "the value of i is :" << i << endl;
+    cout << "the value of j is :" << j << endl;
+    cout << "the value of k is :" << k << endl;
+    // for setw
+    cout << "the value of i is :" << setw(3) << i << endl;
+    cout << "the value of j is :" << setw(3) << j << endl;
+    cout << "the value of k is :" << setw(3) << k << endl;
 }
