1f5faef7dbcdc8e31d200e48f8355da9ba97cff0
harshitverma-dev <theharshitverma53@gmail.com>
2022-11-05 17:16:09 +0530

    reference variable, typecasting

--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    // ************** Reference variables **************  //
     int a = 53;
-    int &b = a;
+    int &b = a; // here b is a reference variable
     cout << a << endl;
     cout << b << endl;
+
+
+    // ************* Typecasting ************* //
+    int x = 56;
+    float y = 78.89;
+    cout << int(y) << endl;
+    cout << float(x) << endl;
 }
