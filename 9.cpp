43b29c20df840016014969b95b073dcbc34aaa49
harshitverma-dev <theharshitverma53@gmail.com>
2022-11-12 20:21:02 +0530

    call by value and call by reference in C++

---
+++ 9.cpp
@@ -0,0 +1,36 @@
+// call by value and call by reference
+#include <iostream>
+using namespace std;
+
+// call by value (Actual and formal arguments will be created in different memory location)
+// int change(int n, int m)
+// {
+//     n = 10;
+//     m = 25;
+// }
+// int main()
+// {
+//     int a = 5;
+//     int b = 10;
+//     change(a, b);
+//     cout << a << endl;
+//     cout << b << endl;
+//     return 0;
+// }
+
+
+// call by reference (Actual and formal arguments will be created in same memory location)
+int change(int* n, int* m)
+{
+    *n = 10;  
+    *m = 25;
+}
+int main()
+{
+    int a = 5;
+    int b = 10;
+    change(&a, &b);  // here we are passing the address of a and b
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
