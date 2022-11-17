e749ecd2b47cd73cf851679274a6b817dc826cce
harshitverma-dev <theharshitverma53@gmail.com>
2022-11-17 05:44:25 +0530

    Default arguments

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int value(int i, int j = 5)  // here j value is the default value , for addition of a and b, if we pass only a value then for b default value will be considered. 
+{
+    return i + j;
+}
+
+int main()
+{
+    int a = 50;
+    int b = 20;
+    cout << "the addition value of a and b :" << value(a) << endl;  // 55
+    cout << "the addition value of a and b :" << value(a, b) << endl;  // 70
+}
