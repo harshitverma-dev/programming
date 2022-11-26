088a33eeae76a7274fa96e4ba38e1ecb1d975112
harshitverma-dev <theharshitverma53@gmail.com>
2022-11-26 13:06:31 +0530

    Default arguments, recursions and recursive functions with examples

--- 10.cpp
+++ 10.cpp
@@ -1,3 +1,4 @@
+// Default arguments
 #include <iostream>
 using namespace std;
 
---
+++ 11.cpp
@@ -0,0 +1,43 @@
+// Recursions and Recursive functions
+// with example >>
+#include <iostream>
+using namespace std;
+
+int fact(int n);
+int fibo(int m);
+
+int fact(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+
+    return n * fact(n - 1);
+}
+
+int fibo(int m)
+{
+    if (m == 0 || m == 1)
+    {
+        return m;
+    }
+
+    return fibo(m - 1) + fibo(m - 2);
+}
+
+int main()
+{
+    int n, m;
+    cout << "Please enter the number to get the factorial:" << endl;
+    cin >> n;
+    cout << "the factorial of " << n << " is: " << fact(n) << endl;
+
+    cout << "Please enter the number to get the fibonacci series:" << endl;
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cout << fibo(i)<<" ";
+    }
+    return 0;
+}
